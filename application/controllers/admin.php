9919a544de496ec1ce0c6e482cfc74fb6245bb50
Rohit Nair <admin@rohitnair.net>
2012-12-13 16:40:53 +0400

    Basic challenge creation code added

--- application/controllers/admin.php
+++ application/controllers/admin.php
@@ -12,7 +12,17 @@ class Admin_Controller extends Base_Controller {
 		return View::make('admin.home')->with('page_title', 'Admin Panel');
 	}
 
-	public function action_challenge(){
+	public function action_challenge( $action = null ){
+
+		if($action == 'new'){
+			$c = new Challenge;
+			$c->name = Input::get('name');
+			$c->start_date = Input::get('start_date');
+			$c->end_date = Input::get('end_date');
+			$c->save();
+			return Redirect::to('/admin/challenge');
+		}
+
 		return View::make('admin.challenge')
 		->with('page_title', 'Admin Panel - Challenges')
 		->with('active_challenges', Challenge::get_active_challenges());
--- application/views/admin/challenge.blade.php
+++ application/views/admin/challenge.blade.php
@@ -4,7 +4,57 @@
 	<div class="row-fluid">
 		@include('admin.sidebar')
 		<div class="span8">
-			<h2>Challenges</h2>
+			<h2>Active Challenges</h2>
+			@if( count($active_challenges) === 0 )
+				<p class="muted">No active challenges found</p>
+			@else
+				<ul>
+					@foreach( $active_challenges as $challenge )
+					<li>
+						<a href="{{ URL::to('/admin/challenge/' . $challenge->id ) }}">
+							{{ $challenge->name }}
+						</a>
+					</li>
+					@endforeach
+				</ul>
+			@endif
+			<div>
+				{{ Form::open('/admin/challenge/new', 'POST', array('class'=>'')) }}
+
+			<h3>Create a new challenge</h3>
+				<div class="control-group">
+					{{ Form::label('name', 'Name', array('class'=>'control-label'))}}
+					<div class="controls">
+						<p>
+							{{ Form::text('name') }}
+						</p>
+					</div>
+				</div>
+
+				<div class="control-group">
+					{{ Form::label('start_date', 'Start Date', array('class'=>'control-label'))}}
+					<div class="controls">
+						<p>
+							{{ Form::text('start_date') }}
+						</p>
+					</div>
+				</div>
+
+				<div class="control-group">
+					{{ Form::label('end_date', 'End Date', array('class'=>'control-label'))}}
+					<div class="controls">
+						<p>
+							{{ Form::text('end_date') }}
+						</p>
+					</div>
+				</div>
+
+				<div>
+					{{ Form::submit('Create', array('class'=>'btn btn-primary')) }}
+				</div>
+
+				{{ Form::close() }}
+			</div>
 		</div>
 	</div>
 @endsection
